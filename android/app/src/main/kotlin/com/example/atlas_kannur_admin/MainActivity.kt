fbb9ac77e8ab9ca5a1f2420c27b7290ddbce831f
thasneemts22 <thasneemts22@gmail.com>
2025-02-27 12:27:10 +0530

    atlas gold admin

---
+++ android/app/src/main/kotlin/com/example/atlas_kannur_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atlas_kannur_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constant/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class useColor {
+  static Color homeIconColor = Color(0xFF34161c);
+  static Color appbarTextWhite = Colors.white;
+}
